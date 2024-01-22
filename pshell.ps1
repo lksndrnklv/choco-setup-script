701d5b8a5e68b38ec204d604904a8fd1298072fc
Aleksandar Nikolov <anikolov18907@gmail.com>
2024-01-23 00:17:35 +0100

    Added git config global prompt and automation

--- pshell.ps1
+++ pshell.ps1
@@ -1,6 +1,10 @@
+$name = Read-Host -Prompt "Enter your full name for git config --global user.name"
+$email = Read-Host -Prompt "Enter your email for git config --global user.email"
 Set-ExecutionPolicy Bypass -Scope Process
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco install git.install -y
+git config --global user.name $name
+git config --global user.email $email
 choco install jdk8 -y
 choco install corretto11jdk -y
 choco install corretto17jdk -y
